a58248b7801acf8850caeec1f553dde55268387e
carocall <2417339414@qq.com>
2026-02-26 14:13:39 +0800

    feat(home): 实现主页布局并添加测试布局

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,5 +1,43 @@
 <template>
-  首页
+  <CommonPage>
+    <template #header-title>
+      主页
+    </template>
+    <template #action-button>
+      <el-button type="primary">新增</el-button>
+      <el-button type="primary">转跳</el-button>
+    </template>
+    <template #body>
+      这是页面主体
+       <el-card style="max-width: 480px">
+    <template #header>Yummy hamburger</template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+  </el-card> <el-card style="max-width: 480px">
+    <template #header>Yummy hamburger</template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+  </el-card> <el-card style="max-width: 480px">
+    <template #header>Yummy hamburger</template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+  </el-card> <el-card style="max-width: 480px">
+    <template #header>Yummy hamburger</template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+  </el-card>
+    </template>
+  </CommonPage>
 </template>
-<script setup></script>
+<script setup>
+import CommonPage from "@/components/common/CommonPage.vue";
+</script>
 <style></style>
