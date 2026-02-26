f97db8a8755134acabf96b9c00b4345f35d1329c
carocall <2417339414@qq.com>
2026-02-26 12:53:39 +0800

    style(FullScreen): 添加按钮样式类并调整图标尺寸
    为全屏按钮添加 function-button 类并定义样式，同时统一图标尺寸为 20x20 像素以保持一致性

--- src/layouts/main/componenet/FullScreen.vue
+++ src/layouts/main/componenet/FullScreen.vue
@@ -5,9 +5,9 @@ const {isFullscreen,toggle} = useFullscreen()
 </script>
 
 <template>
-<i @click="toggle" class="flex-box">
-  <img v-if="isFullscreen" src="@/assets/icon/minimize.svg" alt="minimize">
-  <img  v-else src="@/assets/icon/maximize.svg" alt="maximize">
+<i @click="toggle"  class="flex-box function-button">
+  <img class="icon" v-if="isFullscreen" src="@/assets/icon/minimize.svg" alt="minimize">
+  <img class="icon"  v-else src="@/assets/icon/maximize.svg" alt="maximize">
 </i>
 </template>
 
@@ -16,5 +16,15 @@ const {isFullscreen,toggle} = useFullscreen()
   display: flex;
   align-items: center;
   justify-content: center;
+}
+.function-button{
+  margin-right: 16px;
+  cursor: pointer;
+  height: 20px;
+  width: 20px;
+}
+.icon{
+  height: 20px;
+  width: 20px;
 }
 </style>
