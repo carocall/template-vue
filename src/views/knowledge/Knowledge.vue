e28592b2d49f61e95e3089efb8545155fb836ecb
carocall <2417339414@qq.com>
2026-02-26 16:07:22 +0800

    refactor(页面布局): 使用CommonPage组件重构知识和统计页面
    将知识和统计页面的布局统一使用CommonPage组件重构，提升代码复用性和一致性

--- src/views/knowledge/Knowledge.vue
+++ src/views/knowledge/Knowledge.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>
-        <h1>Knowledge</h1>
-    </div>
+  <CommonPage>
+    <template #header-title>
+      这是知识库界面
+    </template>
+    <template #body>
+      <h1>Knowledge</h1>
+    </template>
+  </CommonPage>
 </template>
 <script setup>
+import CommonPage from "@/components/common/CommonPage.vue";
 </script>
 
 <style>
--- src/views/statistics/Statistics.vue
+++ src/views/statistics/Statistics.vue
@@ -1,9 +1,16 @@
 <template>
-    <div>
-        <h1>Statistics</h1>
-    </div>
+  <CommonPage>
+    <template #header-title>
+      这是数据统计界面
+    </template>
+    <template #body>
+      <h1>statistics</h1>
+    </template>
+  </CommonPage>
+
 </template>
 <script setup>
+import CommonPage from "@/components/common/CommonPage.vue";
 </script>
 
 <style>
