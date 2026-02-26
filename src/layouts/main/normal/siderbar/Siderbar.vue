a914925405e8d0bf94d9d25687294a999ac6e586
carocall <2417339414@qq.com>
2026-02-26 19:37:01 +0800

    style(Siderbar): 添加关于侧边栏宽度溢出的TODO注释

--- src/layouts/main/normal/siderbar/Siderbar.vue
+++ src/layouts/main/normal/siderbar/Siderbar.vue
@@ -16,7 +16,7 @@ const sidebarStore = useSiderbarStore()
 </script>
 
 <style scoped lang="scss">
-
+//TODO 侧边栏宽度变化时,侧边栏宽度会溢出,现在是先隐藏滚动条,等宽度变化完成后再显示滚动条,日后需要修改
 .side-zone {
   height: 100%;
   background: #fff;
