961af429742a6363afea146324fa41260a65b2b0
carocall <2417339414@qq.com>
2026-02-26 14:11:17 +0800

    feat(组件): 添加通用页面布局组件CommonPage
    新增CommonPage组件，提供统一的页面布局结构，包含头部区域和主体内容区域

---
+++ src/components/common/CommonPage.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="common-page">
+
+    <div class="header-card page-header flex-box">
+      <div class="container-left">
+        <slot name="header-title"></slot>
+      </div>
+      <div class="container-right">
+        <slot name="action-button"></slot>
+      </div>
+    </div>
+
+
+    <div class="main-card">
+      <slot name="body"></slot>
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+.common-page {
+  height: 100%;
+  width: 100%;
+}
+
+.page-header {
+  width: 100%;
+
+}
+
+.header-card {
+  width: 100%;
+  height: 50px;
+  background: #fff;
+  border-bottom: 1px solid #e5e7eb;
+  padding: 20px;
+  box-sizing: border-box
+}
+
+.main-card {
+  width: calc(100% - 20px);
+  height: calc(100% - 50px - 20px);
+  margin: 10px;
+  background: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  box-sizing: border-box;
+
+  overflow: auto;
+}
+
+.flex-box {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
