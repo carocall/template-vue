530990d84b6ed1b1278acda16c091680a997b17f
carocall <2417339414@qq.com>
2026-02-26 12:58:03 +0800

    feat(组件): 添加Gitee图标点击跳转功能
    实现Gitee图标点击跳转到指定仓库的功能，并添加相关样式

--- src/layouts/main/componenet/GiteeIcon.vue
+++ src/layouts/main/componenet/GiteeIcon.vue
@@ -1,11 +1,27 @@
 <script setup>
-
+function handleLinkClick(link) {
+  window.open(link)
+}
 </script>
 
 <template>
-  $END$
+  <i
+      class="function-button"
+      @click="handleLinkClick('https://gitee.com/call-caro/template-vue')"
+  >
+    <img src="@/assets/icon/gitee.svg" class="icon-img" alt="gitee">
+  </i>
 </template>
 
 <style scoped>
-
+.function-button{
+  margin-right: 16px;
+  cursor: pointer;
+  height: 20px;
+  width: 20px;
+}
+.icon-img{
+  width: 20px;
+  height: 20px;
+}
 </style>
