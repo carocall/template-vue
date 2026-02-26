1376f48496b71026d185b688bbf5565493c4c39f
carocall <2417339414@qq.com>
2026-02-26 14:37:10 +0800

    feat(用户头像组件): 添加用户名和职位显示样式
    调整用户头像组件布局，新增用户名和职位显示区域，并添加相应样式
    优化flex布局，使内容垂直水平居中

--- src/layouts/main/componenet/UserAvatar.vue
+++ src/layouts/main/componenet/UserAvatar.vue
@@ -5,9 +5,11 @@
 <template>
   <el-dropdown >
     <div class="flex-box">
-      <el-avatar
-          class="avatar" src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500" />
-      <p>admin</p>
+      <el-avatar class="avatar" src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500" />
+      <div>
+        <p class="name">欸嘿嘿</p>
+        <p class="job">[超级管理员]</p>
+      </div>
     </div>
     <template #dropdown>
       <el-dropdown-menu>
@@ -22,8 +24,7 @@
   </el-dropdown>
 </template>
 
-<style scoped>
-
+<style>
 .flex-box {
   display: flex;
   align-items: center;
@@ -35,4 +36,22 @@
   width: 35px;
   margin: 0 10px;
 }
+
+.name {
+  display: flex;
+  align-items: center;    /* 垂直居中 */
+  justify-content: center; /* 水平居中 */
+  font-size: 14px;
+  margin-bottom: 5px;
+  color: #333333;
+}
+
+.job {
+  display: flex;
+  align-items: center;    /* 垂直居中 */
+  justify-content: center; /* 水平居中 */
+  font-size: 10px;
+  color: #808080;
+}
+
 </style>
