b3ce7e02d6081b0171c6c10a5305a8f64ee7031c
carocall <2417339414@qq.com>
2026-02-26 16:32:56 +0800

    feat(用户头像组件): 添加图标并优化下拉菜单样式
    - 导入并使用Element Plus的User和SwitchButton图标
    - 重新组织下拉菜单项，添加图标和样式
    - 优化退出按钮的红色样式
    - 清理并统一代码格式

--- src/layouts/main/componenet/UserAvatar.vue
+++ src/layouts/main/componenet/UserAvatar.vue
@@ -1,11 +1,13 @@
 <script setup>
 
+import {SwitchButton, User} from "@element-plus/icons-vue";
 </script>
 
 <template>
-  <el-dropdown >
+  <el-dropdown>
     <div class="flex-box">
-      <el-avatar class="avatar" src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500" />
+      <el-avatar class="avatar"
+                 src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"/>
       <div>
         <p class="name">欸嘿嘿</p>
         <p class="job">[超级管理员]</p>
@@ -13,11 +15,19 @@
     </div>
     <template #dropdown>
       <el-dropdown-menu>
+
         <el-dropdown-item>
-          退出登录
+          <span>
+            <el-icon><User/></el-icon>
+            个人中心
+          </span>
         </el-dropdown-item>
+
         <el-dropdown-item>
-          个人中心
+          <span class="exit-button">
+            <el-icon><SwitchButton /></el-icon>
+            退出登录
+          </span>
         </el-dropdown-item>
       </el-dropdown-menu>
     </template>
@@ -25,13 +35,20 @@
 </template>
 
 <style>
+
+.exit-button {
+  color: #ff5959 !important;
+  display: flex;
+  align-items: center;
+}
+
 .flex-box {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.avatar{
+.avatar {
   height: 35px;
   width: 35px;
   margin: 0 10px;
@@ -39,7 +56,7 @@
 
 .name {
   display: flex;
-  align-items: center;    /* 垂直居中 */
+  align-items: center; /* 垂直居中 */
   justify-content: center; /* 水平居中 */
   font-size: 14px;
   margin-bottom: 5px;
@@ -48,7 +65,7 @@
 
 .job {
   display: flex;
-  align-items: center;    /* 垂直居中 */
+  align-items: center; /* 垂直居中 */
   justify-content: center; /* 水平居中 */
   font-size: 10px;
   color: #808080;
