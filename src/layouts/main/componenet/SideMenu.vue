23d45d210811b3555cead95cbdf37fc87cbc900d
carocall <2417339414@qq.com>
2026-02-26 16:53:41 +0800

    refactor(layout): 重构侧边栏组件结构
    将 SideMenuItems 组件合并到新的 SideMenu 组件中，简化组件层级结构
    移除冗余的测试菜单项样式

---
+++ src/layouts/main/componenet/SideMenu.vue
@@ -0,0 +1,69 @@
+<script setup>
+import {useRouter} from 'vue-router'
+import {Pointer} from "@element-plus/icons-vue";
+import { useSiderbarStore } from '@/stores/siderbar.js'
+
+const sidebarStore = useSiderbarStore()
+const router = useRouter()
+const selectMenu = (key) => {
+  const currentRoute = router.options.routes[0]
+  router.push(`${currentRoute.path}${key.index}`)
+}
+
+</script>
+
+<template>
+  <el-menu :collapse="sidebarStore.isCollapse" :collapse-transition="false" default-active="2" class="side-menu" >
+  <el-menu-item @click="selectMenu" v-for="item in router.options.routes[0].children" :key="item.path" :index="item.path">
+    <el-icon>
+      <component :is="item.meta.icon"/>
+    </el-icon>
+    <span>{{ item.meta.title }}</span>
+  </el-menu-item>
+
+  <el-menu-item @click="selectMenu">
+    <el-icon>
+      <Pointer/>
+    </el-icon>
+    <span>测试item</span>
+  </el-menu-item>
+
+  <el-menu-item @click="selectMenu">
+    <el-icon>
+      <Pointer/>
+    </el-icon>
+    <span>测试item</span>
+  </el-menu-item>
+  <el-menu-item @click="selectMenu">
+    <el-icon>
+      <Pointer/>
+    </el-icon>
+    <span>测试item</span>
+  </el-menu-item>
+  <el-menu-item @click="selectMenu">
+    <el-icon>
+      <Pointer/>
+    </el-icon>
+    <span>测试item</span>
+  </el-menu-item>
+  <el-menu-item @click="selectMenu">
+    <el-icon>
+      <Pointer/>
+    </el-icon>
+    <span>测试item</span>
+  </el-menu-item>
+  </el-menu>
+
+</template>
+
+<style>
+.side-menu {
+  height: calc(100% - 60px);
+  /* 隐藏滚动条但保持滚动功能 */
+  overflow-y: auto;
+  scrollbar-width: none; /* Firefox */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome/Safari/Edge */
+  }
+}
+</style>
--- src/layouts/main/normal/siderbar/Siderbar.vue
+++ src/layouts/main/normal/siderbar/Siderbar.vue
@@ -1,14 +1,12 @@
 <template>
     <el-aside :width="sidebarStore.isCollapse ? '64px' : '200px'" class="side-zone">
       <SideLogo/>
-        <el-menu :collapse="sidebarStore.isCollapse" :collapse-transition="false" default-active="2" class="side-menu" >
-          <SideMenuItems/>
-        </el-menu>
+      <SideMenu/>
     </el-aside>
 </template>
 
 <script setup>
-import SideMenuItems from "@/layouts/main/componenet/SideMenuItems.vue";
+import SideMenu from "@/layouts/main/componenet/SideMenu.vue";
 import SideLogo from "@/layouts/main/componenet/SideLogo.vue";
 
 import { useSiderbarStore } from '@/stores/siderbar.js'
@@ -18,15 +16,7 @@ const sidebarStore = useSiderbarStore()
 </script>
 
 <style scoped lang="scss">
-.side-menu {
-  height: calc(100% - 60px);
-  /* 隐藏滚动条但保持滚动功能 */
-  overflow-y: auto;
-  scrollbar-width: none; /* Firefox */
-  &::-webkit-scrollbar {
-    display: none; /* Chrome/Safari/Edge */
-  }
-}
+
 .side-zone {
   height: 100%;
 
