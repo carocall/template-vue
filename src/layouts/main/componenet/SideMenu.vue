6953d73d16ac46d70eb86b4e576d6541425df8cd
carocall <2417339414@qq.com>
2026-02-27 15:02:49 +0800

    feat(侧边栏): 重构侧边菜单并支持多级菜单结构
    - 移除硬编码的测试菜单项，使用动态菜单数据
    - 添加对多级菜单的支持，包括子菜单渲染
    - 使用路由路径作为默认激活项
    - 优化菜单项点击处理逻辑

--- src/layouts/main/componenet/SideMenu.vue
+++ src/layouts/main/componenet/SideMenu.vue
@@ -1,57 +1,94 @@
 <script setup>
-import {useRouter} from 'vue-router'
-import {Pointer} from "@element-plus/icons-vue";
+import { useRouter, useRoute } from 'vue-router'
 import { useSiderbarStore } from '@/stores/siderbar.js'
 
 const sidebarStore = useSiderbarStore()
 const router = useRouter()
+const route = useRoute()
+
+
 const selectMenu = (key) => {
-  const currentRoute = router.options.routes[0]
-  router.push(`${currentRoute.path}${key.index}`)
+  router.push(`${key.index}`)
 }
+
+const menuData =  [
+  {
+    code: 'home',
+    name: '首页',
+    path: '/',
+    icon: 'HomeFilled',
+  },
+  {
+    code: 'yunying',
+    name: '运营管理',
+    icon: 'Compass',
+    children: [
+      {
+        code: 'dashboard',
+        name: '数据分析',
+        path: '/dashboard',
+        icon: 'PieChart',
+      },
+      {
+        code: 'knowledge',
+        name: '知识库',
+        path: '/knowledge',
+        icon: 'Document',
+      },
+    ]
+  },
+  {
+    code: 'system',
+    name: '系统管理',
+    icon: 'Setting',
+    children: [
+      {
+        code: 'user',
+        name: '用户管理',
+        path: '/user',
+        icon: 'User',
+      },
+      {
+        code: 'role',
+        name: '角色管理',
+        path: '/role',
+        icon: 'Coin',
+      }
+    ]
+  },
+]
 
 </script>
 
 <template>
-  <el-menu :collapse="sidebarStore.isCollapse" :collapse-transition="false" default-active="2" class="side-menu" >
-  <el-menu-item @click="selectMenu" v-for="item in router.options.routes[0].children" :key="item.path" :index="item.path">
-    <el-icon>
-      <component :is="item.meta.icon"/>
-    </el-icon>
-    <span>{{ item.meta.title }}</span>
-  </el-menu-item>
-
-  <el-menu-item @click="selectMenu">
-    <el-icon>
-      <Pointer/>
-    </el-icon>
-    <span>测试item</span>
-  </el-menu-item>
-
-  <el-menu-item @click="selectMenu">
-    <el-icon>
-      <Pointer/>
-    </el-icon>
-    <span>测试item</span>
-  </el-menu-item>
-  <el-menu-item @click="selectMenu">
-    <el-icon>
-      <Pointer/>
-    </el-icon>
-    <span>测试item</span>
-  </el-menu-item>
-  <el-menu-item @click="selectMenu">
-    <el-icon>
-      <Pointer/>
-    </el-icon>
-    <span>测试item</span>
-  </el-menu-item>
-  <el-menu-item @click="selectMenu">
-    <el-icon>
-      <Pointer/>
-    </el-icon>
-    <span>测试item</span>
-  </el-menu-item>
+  <el-menu :collapse="sidebarStore.isCollapse" :collapse-transition="false" :default-active="route.path" class="side-menu">
+    <!-- 遍历菜单数据 -->
+    <template v-for="item in menuData" :key="item.path">
+      <!-- 如果没有子菜单，直接渲染为普通菜单项 -->
+      <el-menu-item @click="selectMenu" v-if="!item.children" :index="item.path">
+        <el-icon>
+          <component :is="item.icon" />
+        </el-icon>
+        <span>{{ item.name }}</span>
+      </el-menu-item>
+      <!-- 如果有子菜单，渲染为下拉菜单 -->
+      <el-sub-menu v-else :index="item.code">
+        <!-- 多级菜单的标题 -->
+        <template #title>
+          <el-icon>
+            <component :is="item.icon" />
+          </el-icon>
+          <span>{{ item.name }}</span>
+        </template>
+        <!-- 遍历子菜单 -->
+        <el-menu-item @click="selectMenu" v-for="child in item.children" :key="child.path" :index="child.path">
+          <el-icon>
+            <component :is="child.icon" />
+          </el-icon>
+          {{ child.name }}
+        </el-menu-item>
+      </el-sub-menu>
+    </template>
   </el-menu>
 
 </template>
@@ -62,9 +99,12 @@ const selectMenu = (key) => {
   border-right: none;
   /* 隐藏滚动条但保持滚动功能 */
   overflow-y: auto;
-  scrollbar-width: none; /* Firefox */
+  scrollbar-width: none;
+
+  /* Firefox */
   &::-webkit-scrollbar {
-    display: none; /* Chrome/Safari/Edge */
+    display: none;
+    /* Chrome/Safari/Edge */
   }
 }
 </style>
