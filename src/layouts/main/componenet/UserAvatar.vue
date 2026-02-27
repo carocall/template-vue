6aa7d7ccffc9abc230190d2970ddf0d3f9e620e4
carocall <2417339414@qq.com>
2026-02-27 16:53:54 +0800

    style(UserAvatar): 为下拉菜单添加flex-shrink样式防止压缩
    添加user-avatar类名并设置flex-shrink:0，防止用户头像在下拉菜单中被压缩变形

--- src/layouts/main/componenet/UserAvatar.vue
+++ src/layouts/main/componenet/UserAvatar.vue
@@ -4,7 +4,7 @@ import {SwitchButton, User} from "@element-plus/icons-vue";
 </script>
 
 <template>
-  <el-dropdown>
+  <el-dropdown class="user-avatar">
     <div class="flex-box">
       <el-avatar class="avatar"
                  src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"/>
@@ -35,7 +35,9 @@ import {SwitchButton, User} from "@element-plus/icons-vue";
 </template>
 
 <style>
-
+.user-avatar {
+  flex-shrink: 0;
+}
 .exit-button {
   color: #ff5959 !important;
   display: flex;
