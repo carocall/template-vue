acde3b343e86002a68d017f5fc1763de9f8137fe
carocall <2417339414@qq.com>
2026-03-02 12:43:58 +0800

    feat: 添加 pinia 持久化状态插件
    在 pinia 实例中使用 pinia-plugin-persistedstate 插件以实现状态持久化

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import router from '@/router'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
