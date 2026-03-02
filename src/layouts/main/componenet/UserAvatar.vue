33d4cf55c23e0ff4e8bf6b6923873273acecbe6a
carocall <2417339414@qq.com>
2026-03-02 13:59:01 +0800

    feat(用户头像组件): 添加退出登录功能并显示用户信息
    - 引入用户存储和路由，实现退出登录功能
    - 头像和用户名现在从用户存储中动态获取
    - 添加退出成功提示消息

--- src/layouts/main/componenet/UserAvatar.vue
+++ src/layouts/main/componenet/UserAvatar.vue
@@ -1,77 +1,94 @@
 <script setup>
 
 import {SwitchButton, User} from "@element-plus/icons-vue";
+
+import {useUserStore} from "@/stores/user.js";
+import {useRouter} from "vue-router";
+import {ElMessage} from "element-plus";
+const router = useRouter();
+
+const userStore = useUserStore();
+
+const logout = () => {
+    userStore.cleanUserInfo()
+    router.push("/auth/login")
+    ElMessage({
+        type: 'success',
+        message: "成功退出登录",
+    })
+}
+
 </script>
 
 <template>
-  <el-dropdown class="user-avatar">
-    <div class="flex-box">
-      <el-avatar class="avatar"
-                 src="https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"/>
-      <div>
-        <p class="name">欸嘿嘿</p>
-        <p class="job">[超级管理员]</p>
-      </div>
-    </div>
-    <template #dropdown>
-      <el-dropdown-menu>
+    <el-dropdown class="user-avatar">
+        <div class="flex-box">
+            <el-avatar class="avatar"
+                       :src="userStore.userInfo.avatar || 'https://img2.baidu.com/it/u=3422222442,1928892222&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500'"/>
+            <div>
+                <p class="name">{{userStore.userInfo.username || '未命名'}}</p>
+                <p class="job">[管理员]</p>
+            </div>
+        </div>
+        <template #dropdown>
+            <el-dropdown-menu>
 
-        <el-dropdown-item>
-          <span>
+                <el-dropdown-item>
+          <span >
             <el-icon><User/></el-icon>
             个人中心
           </span>
-        </el-dropdown-item>
+                </el-dropdown-item>
 
-        <el-dropdown-item>
-          <span class="exit-button">
+                <el-dropdown-item>
+          <span class="exit-button" @click="logout">
             <el-icon><SwitchButton /></el-icon>
             退出登录
           </span>
-        </el-dropdown-item>
-      </el-dropdown-menu>
-    </template>
-  </el-dropdown>
+                </el-dropdown-item>
+            </el-dropdown-menu>
+        </template>
+    </el-dropdown>
 </template>
 
 <style>
 .user-avatar {
-  flex-shrink: 0;
+    flex-shrink: 0;
     margin: 0 10px;
 }
 .exit-button {
-  color: #ff5959 !important;
-  display: flex;
-  align-items: center;
+    color: #ff5959 !important;
+    display: flex;
+    align-items: center;
 }
 
 .flex-box {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .avatar {
-  height: 35px;
-  width: 35px;
-  margin: 0 10px;
+    height: 35px;
+    width: 35px;
+    margin: 0 10px;
 }
 
 .name {
-  display: flex;
-  align-items: center; /* 垂直居中 */
-  justify-content: center; /* 水平居中 */
-  font-size: 14px;
-  margin-bottom: 5px;
-  color: #333333;
+    display: flex;
+    align-items: center; /* 垂直居中 */
+    justify-content: center; /* 水平居中 */
+    font-size: 14px;
+    margin-bottom: 5px;
+    color: #333333;
 }
 
 .job {
-  display: flex;
-  align-items: center; /* 垂直居中 */
-  justify-content: center; /* 水平居中 */
-  font-size: 10px;
-  color: #808080;
+    display: flex;
+    align-items: center; /* 垂直居中 */
+    justify-content: center; /* 水平居中 */
+    font-size: 10px;
+    color: #808080;
 }
 
 </style>
